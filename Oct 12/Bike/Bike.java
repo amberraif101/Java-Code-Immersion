7bc438b4a3806b0ade04b877e5547949c13ceb42
Amberraif101 <amberraiford103@gmail.com>
2020-10-27 01:59:51 -0400

    -complete

---
+++ Oct 12/Bike/Bike.java
@@ -0,0 +1,17 @@
+public class Bike {
+ 
+    String size;
+    String color;
+    int wheels;
+    int gears;
+    boolean bell;
+
+public Bike(){
+    color = "red";
+    wheels = 2;
+    gears = 8;
+    bell = true;
+
+}
+
+}
---
+++ Oct 12/Bike/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       Bike huffy =  new Bike();
+       
+       System.out.println(huffy.color);
+       System.out.println(huffy.bell);
+       System.out.println(huffy.gears);
+       System.out.println(huffy.wheels);
+    }    
+}
