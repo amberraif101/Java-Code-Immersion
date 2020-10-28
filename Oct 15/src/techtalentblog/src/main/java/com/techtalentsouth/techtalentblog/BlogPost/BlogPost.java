32bb45b85d15a6baa5a98b0c0531f6fed546f043
Amberraif101 <amberraiford103@gmail.com>
2020-10-27 22:04:15 -0400

    add index page including a form to subm blogposts

--- Oct 15/src/techtalentblog/src/main/java/com/techtalentsouth/techtalentblog/BlogPost/BlogPost.java
+++ Oct 15/src/techtalentblog/src/main/java/com/techtalentsouth/techtalentblog/BlogPost/BlogPost.java
@@ -1,6 +1,14 @@
 package com.techtalentsouth.techtalentblog.BlogPost;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class BlogPost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String author;
---
+++ Oct 15/src/techtalentblog/src/main/java/com/techtalentsouth/techtalentblog/BlogPost/BlogPostController.java
@@ -0,0 +1,15 @@
+package com.techtalentsouth.techtalentblog.BlogPost;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogPostController {
+    
+   //localhost:8080 
+
+    @GetMapping(value = "/")
+    public String index(BlogPost blogPost){
+        return "blogpost/index";
+    }
+}
